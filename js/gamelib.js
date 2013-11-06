2a02c030ba76fd0fedae70802367b873906c4603
SpecialKesh <shad0wddeath@gmail.com>
2013-11-06 11:35:48 -0500

    just making code work again
    just fix some syntax errors

--- js/gamelib.js
+++ js/gamelib.js
@@ -12,7 +12,7 @@ function startGame()
 // 1 = male
 // 2 = female
 // 3 = unsure
-{;
+{
 document.getElementById('mainDiv').innerHTML = 
                       "Before we get started are you <br>" +
                       "<button onclick='genderChoice(1)'>MALE</button>" +
@@ -26,26 +26,32 @@ var textbox = "<input type='textbox' id='userName' value=''>";
 var ready = "<button onclick='userName()'>Ready</button>";
 var doneYet = "When you are done entering your name click the button.";
 switch(choice)
+{
 case 1:
 	document.getElementById('mainDiv').innerHTML = 
               "Oh great another male just what we need. <br>" +
               "Ugh, What is your name? <br>" +
               textbox + "<br>" + doneYet + "<br>" + ready;
+break;
 case 2:
 	document.getElementById('mainDiv').innerHTML =
               "Oh hello, you can call me lover! <br>" +
               "What should I call you? <br>" +
                textbox + "<br>" + doneYet + "<br>" + ready;
+break;
 case 3:
 	document.getElementById('mainDiv').innerHTML =
               "Umm okay, this makes my job a bit harder. <br>" +
               "Well what should i call you then? <br>" +
               textbox + "<br>" + doneYet + "<br>" +  ready;
+break;
 case 4:
 	document.getElementById('mainDiv').innerHTML =
               "Okay give me the name you wished to be called," +
               " cause I want to get this show on the road. <br>" +
               textbox + "<br>" + ready;
+break;
+}
 }
 function userName()
 { 
